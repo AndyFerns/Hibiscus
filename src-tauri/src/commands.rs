3a412e055f23b2520e4d52ad7d7b74aa50b19c34
AndyFerns <write2andrew.fernandes@gmail.com>
2026-01-10 04:04:46 +0530

    added load_workspace and save_workspace commands to overall commands module

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::path::PathBuf;
 
+use crate::workspace::WorkspaceFile;
+
 #[tauri::command]
 pub fn read_text_file(path: String) -> Result<String, String> {
     fs::read_to_string(PathBuf::from(path))
@@ -12,3 +14,32 @@ pub fn write_text_file(path: String, contents: String) -> Result<(), String> {
     fs::write(PathBuf::from(path), contents)
         .map_err(|e| e.to_string())
 }
+
+
+/**
+ * Function to load a workspace.json file from directory 
+ */
+#[tauri::command]
+pub fn load_workspace(path: String) -> Result<WorkspaceFile, String> {
+    let content = fs::read_to_string(&path)
+        .map_err(|e| e.to_string())?;
+
+    let workspace: WorkspaceFile =
+        serde_json::from_str(&content)
+            .map_err(|e| e.to_string())?;
+
+    Ok(workspace)
+}
+
+/**
+ * Function to save workspace incase workspace.json doesnt exist
+ */
+#[tauri::command]
+pub fn save_workspace(path: String, workspace: WorkspaceFile) -> Result<(), String> {
+    let json = serde_json::to_string_pretty(&workspace)
+        .map_err(|e| e.to_string())?;
+
+    fs::write(path, json)
+        .map_err(|e| e.to_string())
+}
+
