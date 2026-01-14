8f9a6491081bca21afda27326f406a91622fe7d9
AndyFerns <write2andrew.fernandes@gmail.com>
2026-01-15 02:54:01 +0530

    feat: migrating to manually build monaco and use that instead of using a cdn + minor config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import monacoEditorPlugin from "vite-plugin-monaco-editor"
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    // monaco editor plugin referencing
+    monacoEditorPlugin({
+      // keep this minimal
+      languageWorkers: [
+        "editorWorkerService",
+        "json",
+        "css",
+        "html",
+        "typescript"
+      ]
+    })
+  ],
+
+  // important for tauri-based builds for path resolution
+  base: "./",
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -28,5 +46,9 @@ export default defineConfig(async () => ({
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+  },
+
+  build: {
+    target: "esnext",
   },
 }));
